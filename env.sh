52540791b865936cf41d39eb3bc20b4fcd88de99
Colaplusice <fjl2401@163.com>
2018-06-18 23:35:52 +0800

    change env.sh

--- env.sh
+++ env.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-python manage.py runserver
+#set path for flask config
+export DATABASE_URL='mysql://root:newpass@localhost:3306/hello_flask?charset=utf8mb4'
+export FLASK_CONFIG='production'
+export gname='fjl2401'
+export gpassword='f15114826978f'
+export password1='f15114826978f'
+export name_1='fjl2401@163.com'
+
+python manage.py runserver --host 0.0.0.0
+
 
