b258c8b45d562d060c257fcfaae5a2a91bc27ee1
Colaplusice <fjl2401@163.com>
2018-06-22 10:54:08 +0800

    delete personal message

--- env.sh
+++ env.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 #set path for flask config
-export DATABASE_URL='mysql://root:newpass@localhost:3306/hello_flask?charset=utf8mb4'
+export DATABASE_URL='mysql://root:mysqlpass@localhost:3306/hello_flask?charset=utf8mb4'
 export FLASK_CONFIG='production'
-export gname='fjl2401'
-export gpassword='f15114826978f'
-export password1='f15114826978f'
-export name_1='fjl2401@163.com'
-
+#导入邮箱账号之类的环境变量
 python manage.py runserver --host 0.0.0.0
 
 
