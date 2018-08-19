3e2093284c8b1b6e8558c6be72b081f2ef5c6b76
icecola <jialiang.fan@shanbay.com>
2018-08-19 15:29:49 +0800

    Fixed: 修复了requirements问题 Added: add gunicorn运行命令

---
+++ gunicorn.py
@@ -0,0 +1,39 @@
+import os
+
+workers = int(os.environ.get('WORKERS_NUM', 4))
+
+bind = '0.0.0.0:5000'
+
+worker_class = 'sync'
+
+timeout = 20
+
+limit_request_line = 4096
+
+limit_request_fields = 100
+
+limit_request_field_size = 8190
+
+graceful_timeout = 5
+
+loglevel = 'info'
+
+if os.getenv('FLASK_ENV', 'development') == 'production':
+    loglevel = 'warning'
+
+proc_name = 'hello_flask'
+basedir = os.path.abspath(os.path.dirname(__file__))
+log_dir = os.path.join(basedir, 'logs')
+
+accesslog_path=os.path.join(log_dir,'acesslog.log')
+errorlog_path=os.path.join(log_dir,'errorlog.log')
+
+if not os.path.exists(accesslog_path):
+    with open(accesslog_path,'w'):
+        pass
+if not os.path.exists(errorlog_path):
+    with open(errorlog_path,'w'):
+        pass
+
+accesslog = accesslog_path
+errorlog = errorlog_path
